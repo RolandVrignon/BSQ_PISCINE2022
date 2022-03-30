1ac7c7f4abd5346deba33fd5c8b430cd940661d1
Elie ANNESTAY <eannesta@e3r8p20.clusters.42paris.fr>
2022-03-30 21:01:52 +0200

    minor changes

--- main.c
+++ main.c
@@ -14,10 +14,29 @@
 #include <stdio.h>
 #include "lib/utils.h"
 
+char	*parser(int file)
+{
+	int 	fd;
+	char 	ch;
+	char 	*filename;
+	
+	filename = malloc(sizeof(char) * 9);
+	filename = "./tmp.ox";
+	fd = open(filename, O_WRONLY | O_APPEND | O_CREAT, 0644);
+	if (fd == -1)
+		return (0);
+	while(read(file, &ch, 1) != 0 && ch != EOF)
+		write(fd, &ch, 1);
+	close(fd);
+	return (filename);
+}
+
+
 int	main(int ac, char **av)
 {
 	char	**lines;
 	int		i;
+	char 	*file;
 
 	if (ac >= 2)
 	{
@@ -25,16 +44,18 @@ int	main(int ac, char **av)
 		while (av[i])
 		{
 			lines = open_file(av[i]);
-			if (!check(lines))
-				i++;
-			else
-			{
+			if (check(lines))
 				make_process(lines);
-				i++;
-			}
+			i++;
 		}
 	}
 	else if (ac == 1)
-		write(1, "Hello\n", 6);
+	{
+		file = parser(0);
+		lines = open_file(file);
+		write(1, "\n\n", 2);
+		if (check(lines))
+			make_process(lines);
+	}
 	return (0);
 }
